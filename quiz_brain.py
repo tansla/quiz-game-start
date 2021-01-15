66c011961b7fad1f16a585d228f7440282ec5459
tansla <51797519+tansla@users.noreply.github.com>
2021-01-15 12:35:38 +0300

    corrected "current score"

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,30 +1,30 @@
 class QuizBrain:
 
     def __init__(self, questions):
-        self.question_number = 0
-        self.score = 0
-        self.question_list = questions
+        self._question_number = 0
+        self._score = 0
+        self._question_list = questions
 
     def still_has_questions(self):
-        return self.question_number < len(self.question_list)
+        return self._question_number < len(self._question_list)
 
     def next_question(self):
-        if len(self.question_list) >= self.question_number:
-            question = self.question_list[self.question_number]
-            user_answer = input(f"Q.{self.question_number}: {question.text} (True/False)?").lower()
-            self.question_number += 1
+        if len(self._question_list) >= self._question_number:
+            question = self._question_list[self._question_number]
+            user_answer = input(f"Q.{self._question_number}: {question.text} (True/False)?").lower()
+            self._question_number += 1
             self.check_answer(user_answer, question.answer)
         else:
             return "There is no more questions"
 
     def check_answer(self, u_answer, correct_answer):
         if u_answer.lower() == str(correct_answer).lower():
-            self.score += 1
+            self._score += 1
             print("You right!")
         else:
             print(f"You're wrong! The answer was: {str(correct_answer)}")
-        print(f"Your current score: {self.score}/{len(self.question_list)}")
+        print(f"Your current score: {self._score}/{self._question_number}")
         print("\n")
 
     def final_score(self):
-        return f"You're final score: {self.score}/{len(self.question_list)}"
+        return f"You're final score: {self._score}/{len(self._question_list)}"
